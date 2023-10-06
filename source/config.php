82ce0b91abf9b6f95decfadc5c1d2b077ab901e6
Awes <28003@ma-web.nl>
2023-10-06 15:42:23 +0200

    MusicLibraryV3.0

--- source/config.php
+++ source/config.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM artist";
+$sql = "SELECT * FROM muziek";
 $result = $conn->query($sql);
 
-//if ($result->num_rows > 0) {
+  $library = [];
   // output data of each row
   while($row = $result->fetch_assoc()) {
     //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-  var_dump($row);
+   $library[] =$row;
 
 }
--- source/views/card.php
+++ source/views/card.php
@@ -1,8 +1,8 @@
 <section class="muziek">
-    <div class="background-image"></div>
+    
     <div class="gradient-overlay"></div>
     <h1 class="artist-name">Titel: <?php echo $artist['title'] ?></h1>
-    <h2 class="artist-description">Beschrijving: <?php echo $artist['artist'] ?></h2>
+    <h2 class="artist-description">Artiest: <?php echo $artist['artist'] ?></h2>
     <h3 class="artist-genre">Genre: <?php echo $artist['genre']; ?></h3>
 
     </section>
